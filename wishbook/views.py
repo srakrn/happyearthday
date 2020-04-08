e70daec07bf6d44ce2eed15e501d37dfdcd901e3
Sirakorn Lamyai <tanersirakorn@gmail.com>
2020-04-08 22:20:46 +0700

    From Khamin to Earth

--- wishbook/views.py
+++ wishbook/views.py
@@ -26,7 +26,7 @@ def new_wish(request):
                 messages.SUCCESS,
                 """ส่งคำอวยพรให้ขมิ้นสำเร็จแล้ว! ทวิตอวยพรด้วยข้อความเมื่อกี้ได้ด้วยการกดปุ่มนี้ 
                     <a class="twitter-share-button"
-                    href='https://twitter.com/intent/tweet?url=hbdcherprang.herokupp.com&text={}%20#Born2BeKhamin'>Tweet</a></p>""".format(
+                    href='https://twitter.com/intent/tweet?url=hbdcherprang.herokupp.com&text={}%20#HappyEarthDay'>Tweet</a></p>""".format(
                     wish.wish_text
                 ),
             )
